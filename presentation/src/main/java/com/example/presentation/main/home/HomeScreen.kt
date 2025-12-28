04c0f92a292062f855120df89329a571cf93b94f
Juhyeok <zero5.two4@gmail.com>
2025-12-28 15:24:22 +0900

    HomeScreen 위치 권한 로직 추가

--- presentation/src/main/java/com/example/presentation/main/home/HomeScreen.kt
+++ presentation/src/main/java/com/example/presentation/main/home/HomeScreen.kt
@@ -1,16 +1,29 @@
 package com.example.presentation.main.home
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -23,62 +36,97 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
+    var hasLocationPermission by remember {
+        mutableStateOf(checkLocationPermission(context))
+    }
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        hasLocationPermission = isGranted
+
+        if (isGranted) {
+            viewModel.loadCurrentLocation()
+        } else {
+            viewModel.useDefaultLocation()
+        }
+    }
+
+    LaunchedEffect(hasLocationPermission) {
+        if (hasLocationPermission) {
+            viewModel.loadCurrentLocation()
+        } else {
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            viewModel.useDefaultLocation()
+        }
+    }
+
+    LaunchedEffect(lifecycleOwner) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
+            hasLocationPermission = checkLocationPermission(context)
+        }
+    }
+
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.sideEffect.collect {
+            when (it) {
+                is HomeSideEffect.Toast -> {
+                    Toast.makeText(context, it.message, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
     HomeScreen(
+        isMyLocationEnabled = hasLocationPermission,
         homeState = uiState
     )
+}
+
+private fun checkLocationPermission(context: Context): Boolean {
+    return ContextCompat.checkSelfPermission(
+        context,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 }
 
 @Composable
 private fun HomeScreen(
     modifier: Modifier = Modifier,
+    isMyLocationEnabled: Boolean = false,
     homeState: HomeUiState,
 ) {
+
     Surface {
-        when (homeState) {
-            is HomeUiState.Loading -> {
-                Box(modifier = modifier.fillMaxSize()) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            }
-
-            is HomeUiState.Success -> {
-                ParkingLotMap(
-                    modifier = Modifier.fillMaxSize(),
-                    currentLocation = LatLng(
-                        homeState.currentLocation.latitude,
-                        homeState.currentLocation.longitude
-                    )
-                )
-            }
-
-            is HomeUiState.Error -> {
-                ParkingLotMap(
-                    modifier = Modifier.fillMaxSize(),
-                )
-            }
-        }
+        ParkingLotMap(
+            modifier = Modifier.fillMaxSize(),
+            isMyLocationEnabled = isMyLocationEnabled,
+            currentLocation = LatLng(
+                homeState.currentLocation.latitude,
+                homeState.currentLocation.longitude
+            )
+        )
     }
 }
 
 @Composable
 private fun ParkingLotMap(
     modifier: Modifier = Modifier,
-    currentLocation: LatLng? = null,
+    isMyLocationEnabled: Boolean,
+    currentLocation: LatLng,
 ) {
-    val defaultLocation = LatLng(37.566535, 126.9779)
-    val mapLocation = currentLocation ?: defaultLocation
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(mapLocation, 15f)
+        position = CameraPosition.fromLatLngZoom(currentLocation, 15f)
     }
 
     LaunchedEffect(currentLocation) {
-        currentLocation?.let {
-            cameraPositionState.position = CameraPosition.fromLatLngZoom(it, 15f)
-        }
+        cameraPositionState.animate(
+            update = CameraUpdateFactory.newLatLngZoom(currentLocation, 15f),
+            durationMs = 500
+        )
     }
 
     Box(modifier) {
@@ -86,10 +134,10 @@ private fun ParkingLotMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState,
             properties = MapProperties(
-                isMyLocationEnabled = currentLocation != null
+                isMyLocationEnabled = isMyLocationEnabled
             ),
             uiSettings = MapUiSettings(
-                myLocationButtonEnabled = true,
+                myLocationButtonEnabled = isMyLocationEnabled,
                 zoomControlsEnabled = true,
                 zoomGesturesEnabled = true
             )
--- presentation/src/main/java/com/example/presentation/main/home/HomeViewModel.kt
+++ presentation/src/main/java/com/example/presentation/main/home/HomeViewModel.kt
@@ -4,11 +4,15 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.Location
 import com.example.domain.usecase.main.home.GetCurrentLocationUseCase
+import com.google.android.gms.maps.model.LatLng
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -17,33 +21,42 @@ class HomeViewModel @Inject constructor(
     private val getCurrentLocationUseCase: GetCurrentLocationUseCase
 ) : ViewModel() {
 
-    private val _uiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState.Loading)
+    private val _uiState: MutableStateFlow<HomeUiState> = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
 
-    init {
-        getCurrentLocation()
-    }
+    private val _sideEffect: MutableSharedFlow<HomeSideEffect> = MutableSharedFlow(replay = 0)
+    val sideEffect: SharedFlow<HomeSideEffect> = _sideEffect.asSharedFlow()
 
-    private fun getCurrentLocation() {
-        viewModelScope.launch(Dispatchers.IO) {
+    fun loadCurrentLocation() {
+        viewModelScope.launch {
             getCurrentLocationUseCase().fold(
                 onSuccess = { location ->
-                    _uiState.value = HomeUiState.Success(currentLocation = location)
+                    _uiState.update {
+                        it.copy(currentLocation = location.run { LatLng(latitude, longitude) })
+                    }
                 },
-                onFailure = {
-                    _uiState.value = HomeUiState.Error
-                    // todo side effect toast
+                onFailure = { exception ->
+                    val message = when (exception) {
+                        is SecurityException -> "위치 권한이 필요합니다."
+                        else -> "위치를 가져올 수 없습니다."
+                    }
+                    _sideEffect.emit(HomeSideEffect.Toast(message))
                 }
             )
         }
     }
+
+    fun useDefaultLocation() {
+        _uiState.update {
+            it.copy(currentLocation = Location.DEFAULT.run { LatLng(latitude, longitude) })
+        }
+    }
 }
 
-interface HomeUiState {
-    object Loading : HomeUiState
-    data class Success(
-        val currentLocation: Location,
-    ) : HomeUiState
+data class HomeUiState(
+    val currentLocation: LatLng = Location.DEFAULT.run { LatLng(latitude, longitude) },
+)
 
-    object Error : HomeUiState
+sealed interface HomeSideEffect {
+    data class Toast(val message: String) : HomeSideEffect
 }
